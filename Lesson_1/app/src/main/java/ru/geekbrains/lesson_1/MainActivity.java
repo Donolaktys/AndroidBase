dd671af0bac85c9f4cae12145fbb10e42969d99b
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-04-11 22:24:38 +0300

    Заготовка для обработки кнопок на первом экране

--- Lesson_1/app/src/main/java/ru/geekbrains/lesson_1/MainActivity.java
+++ Lesson_1/app/src/main/java/ru/geekbrains/lesson_1/MainActivity.java
@@ -2,12 +2,36 @@ package ru.geekbrains.lesson_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+private TextView locality;
+private TextView infoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locality = findViewById(R.id.locality);
+        infoText = findViewById(R.id.infoText);
+        Button oneDayBtn  = findViewById(R.id.oneDayBtn);
+        Button threeDaysBtn  = findViewById(R.id.threeDaysBtn);
+        Button weekBtn  = findViewById(R.id.weekBtn);
+
+        oneDayBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка 1 день");
+        });
+
+        threeDaysBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка 3 дня");
+        });
+
+        weekBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка неделя");
+        });
     }
+
+
 }
