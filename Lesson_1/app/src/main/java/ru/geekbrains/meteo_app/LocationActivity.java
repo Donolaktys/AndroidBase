206ccc14fe4e04fcb75c2552a29d74b671709319
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-04-12 12:06:56 +0300

    rename packages. Для поля enterLocation параметр text заменен на hint

---
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/LocationActivity.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.meteo_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class LocationActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        String[] city = {"Москва", "Воронеж", "Смоленск"};
+
+        ListView lvCity = (ListView) findViewById(R.id.lvCity);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, city);
+
+        lvCity.setAdapter(adapter);
+
+
+    }
+
+
+}
---
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.meteo_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+private Button locality;
+private TextView infoText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locality = findViewById(R.id.locality);
+        infoText = findViewById(R.id.infoText);
+        Button oneDayBtn  = findViewById(R.id.oneDayBtn);
+        Button threeDaysBtn  = findViewById(R.id.threeDaysBtn);
+        Button weekBtn  = findViewById(R.id.weekBtn);
+
+        locality.setOnClickListener(v -> {
+        Intent locationIntent = new Intent(MainActivity.this, LocationActivity.class);
+
+        startActivity(locationIntent);
+        });
+
+        oneDayBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка 1 день");
+        });
+
+        threeDaysBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка 3 дня");
+        });
+
+        weekBtn.setOnClickListener(v -> {
+            infoText.setText("Нажата кнопка неделя");
+        });
+    }
+
+
+}
