0a078484e37738c78814bc050ba37f527b25574e
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-04-19 17:34:53 +0300

    Возврат местоположания из окна выбора на главный

---
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/Constants.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.meteo_app;
+
+public interface Constants {
+    public final String CITY = "ru.geekbrains.meteo_app.city";
+}
--- Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/MainActivity.java
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/MainActivity.java
@@ -1,24 +1,24 @@
 package ru.geekbrains.meteo_app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity implements Constants{
+    private TextView localityChoice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MakeLog.lifeCycle(this,"Created");
+        MakeLog.lifeCycle(this,"MAIN Created");
 
-        TextView localityChoice = findViewById(R.id.localityChoice);
+        localityChoice = findViewById(R.id.localityChoice);
         ImageButton settingsBtn = findViewById(R.id.settingsBtn);
         Button oneDayBtn  = findViewById(R.id.oneDayBtn);
         Button threeDaysBtn  = findViewById(R.id.threeDaysBtn);
@@ -29,8 +29,7 @@ public class MainActivity extends Activity {
             // выбор местоположения, открытие экрана выбора
         localityChoice.setOnClickListener(v -> {
         Intent locationIntent = new Intent(MainActivity.this, LocationActivity.class);
-
-        startActivity(locationIntent);
+            startActivityForResult(locationIntent, 1);
         });
 
             //обработка нажатия кнопок выбора вариантов отображения
@@ -50,57 +49,65 @@ public class MainActivity extends Activity {
         settingsBtn.setOnClickListener(v -> {
             MakeLog.click(this,"\"Настройки\"");
             Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
-
             startActivity(settingsIntent);
         });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MakeLog.lifeCycle(getApplicationContext(), "onActivityResult");
+        if (data == null) {return;}
+        String city = data.getStringExtra(CITY);
+        localityChoice.setText(city);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        MakeLog.lifeCycle(this,"Started");
+        MakeLog.lifeCycle(this,"MAIN Started");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MakeLog.lifeCycle(this,"Resumed");
+        MakeLog.lifeCycle(this,"MAIN Resumed");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        MakeLog.lifeCycle(this,"Paused");
+        MakeLog.lifeCycle(this,"MAIN Paused");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        MakeLog.lifeCycle(this,"Stopped");
+        MakeLog.lifeCycle(this,"MAIN Stopped");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        MakeLog.lifeCycle(this,"Restarted");
+        MakeLog.lifeCycle(this,"MAIN Restarted");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        MakeLog.lifeCycle(this,"Destroyed");
+        MakeLog.lifeCycle(this,"MAIN Destroyed");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        MakeLog.lifeCycle(this, "Сохранение данных");
+        MakeLog.lifeCycle(this, "MAIN Сохранение данных");
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        MakeLog.lifeCycle(this, "Восстановление данных");
+        MakeLog.lifeCycle(this, "MAIN Восстановление данных");
     }
 
 }
