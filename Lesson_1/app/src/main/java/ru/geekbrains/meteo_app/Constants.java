ee9e6f5fcb15c7db6bfa417fe560972bf9971c38
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-04-21 13:24:55 +0300

    Обработка результатов onActivityResult

--- Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/Constants.java
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/Constants.java
@@ -2,4 +2,5 @@ package ru.geekbrains.meteo_app;
 
 public interface Constants {
     public final String CITY = "ru.geekbrains.meteo_app.city";
+    public final int REQUEST_CODE_CITY = 1;
 }
--- Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/MainActivity.java
+++ Lesson_1/app/src/main/java/ru/geekbrains/meteo_app/MainActivity.java
@@ -12,53 +12,54 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements Constants{
+public class MainActivity extends AppCompatActivity implements Constants {
     private TextView localityChoice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MakeLog.lifeCycle(this,"MAIN Created");
+        MakeLog.lifeCycle(this, "MAIN Created");
 
         localityChoice = findViewById(R.id.localityChoice);
         ImageButton settingsBtn = findViewById(R.id.settingsBtn);
-        Button oneDayBtn  = findViewById(R.id.oneDayBtn);
-        Button threeDaysBtn  = findViewById(R.id.threeDaysBtn);
-        Button weekBtn  = findViewById(R.id.weekBtn);
+        Button oneDayBtn = findViewById(R.id.oneDayBtn);
+        Button threeDaysBtn = findViewById(R.id.threeDaysBtn);
+        Button weekBtn = findViewById(R.id.weekBtn);
         TextView infoLink = findViewById(R.id.infoLink);
         String link = getApplicationContext().getString(R.string.link);
 
         //обработка нажатия кнопок на первом экране
-            //
-            // выбор местоположения, открытие экрана выбора
+        //
+        // выбор местоположения, открытие экрана выбора
         localityChoice.setOnClickListener(v -> {
-        Intent locationIntent = new Intent(MainActivity.this, LocationActivity.class);
-            startActivityForResult(locationIntent, 1);
+            Intent locationIntent = new Intent(MainActivity.this, LocationActivity.class);
+            startActivityForResult(locationIntent, REQUEST_CODE_CITY);
         });
 
-            //дополнительная информация
+        //дополнительная информация
         infoLink.setOnClickListener(v -> {
             Intent info = new Intent(Intent.ACTION_VIEW, Uri.parse(link + localityChoice.getText()));
             startActivity(info);
         });
 
-            //обработка нажатия кнопок выбора вариантов отображения
+        //обработка нажатия кнопок выбора вариантов отображения
         oneDayBtn.setOnClickListener(v -> {
-            MakeLog.click(this,"\"1 день\"");
+            MakeLog.click(this, "\"1 день\"");
         });
 
         threeDaysBtn.setOnClickListener(v -> {
-            MakeLog.click(this,"\"3 дня\"");
+            MakeLog.click(this, "\"3 дня\"");
         });
 
         weekBtn.setOnClickListener(v -> {
-            MakeLog.click(this,"\"неделя\"");
+            MakeLog.click(this, "\"неделя\"");
         });
 
-            // нажатие на кнопку настроек, переход на экран настройки приложения
+        // нажатие на кнопку настроек, переход на экран настройки приложения
         settingsBtn.setOnClickListener(v -> {
-            MakeLog.click(this,"\"Настройки\"");
+            MakeLog.click(this, "\"Настройки\"");
             Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(settingsIntent);
         });
@@ -68,45 +69,51 @@ public class MainActivity extends AppCompatActivity implements Constants{
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         MakeLog.lifeCycle(getApplicationContext(), "onActivityResult");
-        if (data == null) {return;}
-        String city = data.getStringExtra(CITY);
-        localityChoice.setText(city);
+        if (data == null) {
+            return;
+        }
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_CITY) {
+            String city = data.getStringExtra(CITY);
+            localityChoice.setText(city);
+        } else {
+            MakeLog.error(getApplicationContext(), "Wrong result");
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        MakeLog.lifeCycle(this,"MAIN Started");
+        MakeLog.lifeCycle(this, "MAIN Started");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        MakeLog.lifeCycle(this,"MAIN Resumed");
+        MakeLog.lifeCycle(this, "MAIN Resumed");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        MakeLog.lifeCycle(this,"MAIN Paused");
+        MakeLog.lifeCycle(this, "MAIN Paused");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        MakeLog.lifeCycle(this,"MAIN Stopped");
+        MakeLog.lifeCycle(this, "MAIN Stopped");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        MakeLog.lifeCycle(this,"MAIN Restarted");
+        MakeLog.lifeCycle(this, "MAIN Restarted");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        MakeLog.lifeCycle(this,"MAIN Destroyed");
+        MakeLog.lifeCycle(this, "MAIN Destroyed");
     }
 
     @Override
